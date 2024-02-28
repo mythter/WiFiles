99abb768cfb93f2327b5c2b28fcf400d5dc284c7
Anton <anton260903@gmail.com>
2024-02-28 22:55:35 +0200

    Add project files.

---
+++ Server/Hubs/FileHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server.Hubs
+{
+    public class FileHub : Hub
+    {
+        public async Task SendingFile(byte[] chunk, string whomId)
+        {
+            await Clients.Client(whomId).SendAsync("ReceivingFile", chunk);
+        }
+
+        public async Task StartSendingFile(string fileName, long length, string whomId)
+        {
+            await Clients.Client(whomId).SendAsync("StartReceivingFile", fileName, length);
+        }
+
+        public async Task EndSendingFile(string whomId)
+        {
+            await Clients.Client(whomId).SendAsync("EndReceivingFile");
+        }
+    }
+}
