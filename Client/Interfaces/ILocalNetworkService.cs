8aedf67c060a26821b8f26e4ee201c76630cef15
Anton <anton260903@gmail.com>
2024-03-17 23:41:16 +0200

    interfaces and services moved to Client project

---
+++ Client/Interfaces/ILocalNetworkService.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace BLL.Interfaces
+{
+    public interface ILocalNetworkService
+    {
+        //List<IPAddress> GetAllHostIpAddresses();
+
+        List<IPAddress> GetAllHostIpAddressesWithGateway();
+        IPAddress? GetGatewayByIp(IPAddress ip);
+
+        //IPAddress? GetGatewayByHostIp(IPAddress ip);
+        IPAddress? GetSubnetMaskByIp(IPAddress ip);
+        //IPAddress? GetSubnetMaskByHostIp(IPAddress ip);
+        IPAddress GetNetworkAddress(IPAddress ip, IPAddress mask);
+    }
+}
