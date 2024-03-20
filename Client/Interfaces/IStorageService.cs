226902e83fcb65d684bcf1a3fb924607ffabbf39
Anton <anton260903@gmail.com>
2024-03-21 00:16:09 +0200

    IStorageService added

---
+++ Client/Interfaces/IStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.Interfaces
+{
+    public interface IStorageService
+    {
+        public Task<List<string>> PickFilesAsync();
+        public Task<string?> PickFolderAsync();
+    }
+}
