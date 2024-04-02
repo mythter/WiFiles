9973bb3b90c1c6a670feba90235f334929f547c1
Anton <anton260903@gmail.com>
2024-04-03 00:08:13 +0300

    DeviceComponent added

---
+++ Client/Components/Pages/Items/DeviceComponent.razor.js
@@ -0,0 +1,10 @@
+﻿export class Device {
+    static Init(device, sendBtn) {
+        device.addEventListener("click", () => {
+            sendBtn.classList.remove("hidden");
+        });
+        device.addEventListener("focusout", () => {
+            sendBtn.classList.add("hidden");
+        });
+    }
+}
