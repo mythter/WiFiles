887bef9006567d4f1aa1d33faf8508595be62476
Anton <anton260903@gmail.com>
2024-04-08 20:54:11 +0300

    FileModel added

---
+++ Client/Models/FileModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Client.Models
+{
+    public class FileModel
+    {
+        public string Path { get; set; }
+        public long Size { get; set; }
+        public IPAddress? Sender { get; set; }
+        public Progress<long>? Progress { get; set; }
+
+        public FileModel(string path, long size, IPAddress? sender = null, Progress<long>? progress = null)
+        {
+            Path = path;
+            Size = size;
+            Sender = sender;
+            Progress = progress;
+        }
+    }
+}
