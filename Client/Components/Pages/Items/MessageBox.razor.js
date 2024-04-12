3cd6dd9241111f326bdd40be96fb1e6ef295e04a
Anton <anton260903@gmail.com>
2024-04-12 22:06:29 +0300

    MessageBox added

---
+++ Client/Components/Pages/Items/MessageBox.razor.js
@@ -0,0 +1,11 @@
+﻿export class MessageBox {
+    static Init(dotNet, container) {
+        if (container) {
+            container.addEventListener('animationend', (e) => {
+                if (e.animationName.startsWith("disappearing")) {
+                    dotNet.invokeMethodAsync("Clear");
+                }
+            });
+        }
+    }
+}
