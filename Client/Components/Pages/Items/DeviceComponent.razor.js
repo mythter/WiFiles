61dca332fe02860391aef2b354ad8fed7cf68743
Anton <anton260903@gmail.com>
2024-04-15 23:24:23 +0300

    some DeviceComponent logic rewritten

--- Client/Components/Pages/Items/DeviceComponent.razor.js
+++ Client/Components/Pages/Items/DeviceComponent.razor.js
@@ -1,12 +1,12 @@
-﻿export class Device {
-    static Init(device, sendBtn) {
-        if (device) {
-            device.addEventListener("click", () => {
-                sendBtn.classList.remove("hidden");
-            });
-            device.addEventListener("focusout", () => {
-                sendBtn.classList.add("hidden");
-            });
-        }
-    }
-}
+﻿//export class Device {
+//    static Init(device, sendBtn) {
+//        if (device) {
+//            device.addEventListener("click", () => {
+//                sendBtn.classList.remove("hidden");
+//            });
+//            device.addEventListener("focusout", () => {
+//                sendBtn.classList.add("hidden");
+//            });
+//        }
+//    }
+//}
