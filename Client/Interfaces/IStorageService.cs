1dfa85ea5662fc24d1aedafcae6d0daf65eee2f0
Anton <anton260903@gmail.com>
2024-04-19 16:48:19 +0300

    IStorageService CheckIsDirectoryWritable and CheckIsFileReadable methods added

--- Client/Interfaces/IStorageService.cs
+++ Client/Interfaces/IStorageService.cs
@@ -5,5 +5,7 @@
         public Task<List<string>> PickFilesAsync();
         public Task<string?> PickFolderAsync();
         public string? GetDefaultFolder();
+        public bool CheckIsDirectoryWritable(string dirPath, bool throwIfFails = false);
+        public bool CheckIsFileReadable(string filePath, bool throwIfFails = false);
     }
 }
--- Client/Services/WindowsStorageService.cs
+++ Client/Services/WindowsStorageService.cs
@@ -5,6 +5,39 @@ namespace Client.Services
 {
     public class WindowsStorageService : IStorageService
     {
+        public bool CheckIsDirectoryWritable(string dirPath, bool throwIfFails = false)
+        {
+            try
+            {
+                string fileName = Path.Combine(dirPath, Path.GetRandomFileName());
+                using FileStream fs = File.Create(fileName, 1, FileOptions.DeleteOnClose);
+                return true;
+            }
+            catch
+            {
+                if (throwIfFails)
+                    throw;
+                else
+                    return false;
+            }
+        }
+
+        public bool CheckIsFileReadable(string filePath, bool throwIfFails = false)
+        {
+            try
+            {
+                using FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read);
+                return true;
+            }
+            catch
+            {
+                if (throwIfFails)
+                    throw;
+                else
+                    return false;
+            }
+        }
+
         public string? GetDefaultFolder()
         {
             string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
