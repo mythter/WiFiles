41591cdea40e87ac4f5cc178e0068206cd11c83c
Anton <anton260903@gmail.com>
2024-11-10 19:21:57 +0200

    NetworkStreamExtensions some new methods added to read primitive types

--- Client/Extensions/NetworkStreamExtensions.cs
+++ Client/Extensions/NetworkStreamExtensions.cs
@@ -1,22 +1,20 @@
 ﻿using System.Net.Sockets;
+using System.Text;
 
 namespace Client.Extensions
 {
     public static class NetworkStreamExtensions
     {
         public static async Task<int> ReadWithTimeoutAsync(
-            this NetworkStream stream, 
-            byte[] buffer, 
-            int timeout, 
+            this NetworkStream stream,
+            byte[] buffer,
+            int timeout,
             CancellationToken cancellationToken)
         {
             using var readCts = new CancellationTokenSource(TimeSpan.FromMilliseconds(timeout));
             using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(readCts.Token, cancellationToken);
 
             return await stream.ReadAsync(buffer, linkedCts.Token);
-            }
-
-            return await readTask;
         }
 
         public static async Task WriteWithTimeoutAsync(
@@ -29,18 +27,45 @@ namespace Client.Extensions
             using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(writeCts.Token, cancellationToken);
 
             await stream.WriteAsync(buffer, linkedCts.Token);
+        }
 
-            if (!writeTask.IsCompletedSuccessfully)
-            {
-                if (writeTask.Exception?.InnerException?.InnerException is SocketException ex)
-                {
-                    throw ex;
-                }
+        public static async Task<T> ReadAsync<T>(
+            this NetworkStream stream,
+            Func<byte[], T> converter,
+            int size,
+            CancellationToken cancellationToken = default)
+        {
+            byte[] buffer = new byte[size];
+            await stream.ReadExactlyAsync(buffer, 0, size);
+            return converter(buffer);
+        }
 
-                throw new OperationCanceledException("Receiver or you disconnected.");
-            }
+        public static async Task<bool> ReadBooleanAsync(
+            this NetworkStream stream,
+            CancellationToken cancellationToken = default)
+        {
+            return await stream.ReadAsync(
+                (bytes) => BitConverter.ToBoolean(bytes, 0),
+                sizeof(bool),
+                cancellationToken);
+        }
 
-            await writeTask;
+        public static async Task<int> ReadInt32Async(
+            this NetworkStream stream,
+            CancellationToken cancellationToken = default)
+        {
+            return await stream.ReadAsync(
+                (bytes) => BitConverter.ToInt32(bytes, 0),
+                sizeof(int),
+                cancellationToken);
+        }
+
+        public static async Task<string> ReadStringAsync(
+            this NetworkStream stream,
+            int size,
+            CancellationToken cancellationToken = default)
+        {
+            return await stream.ReadAsync(Encoding.UTF8.GetString, size, cancellationToken);
         }
     }
 }
