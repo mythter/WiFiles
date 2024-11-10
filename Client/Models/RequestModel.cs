801d9011ca71b7d59a3edead49d57107450798f3
Anton <anton260903@gmail.com>
2024-11-10 19:08:10 +0200

    RequestModel and FileMetadata models added

---
+++ Client/Models/RequestModel.cs
@@ -0,0 +1,15 @@
+﻿using Client.Enums;
+
+namespace Client.Models
+{
+    public class RequestModel
+    {
+        public string DeviceName { get; set; }
+
+        public string? DeviceModel { get; set; }
+
+        public DeviceModelType DeviceType { get; set; }
+
+        public List<FileMetadata> Files { get; set; } = new();
+    }
+}
