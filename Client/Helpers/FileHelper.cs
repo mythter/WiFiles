52a05c1b2b5e26268ef428d6d4093804a1fe3727
Anton <anton260903@gmail.com>
2024-12-08 22:13:59 +0200

    GetUniqueFilePath method moved to FileHelper class

---
+++ Client/Helpers/FileHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Client.Helpers
+{
+    public static class FileHelper
+    {
+        public static string GetUniqueFilePath(string fileName, string directory)
+        {
+            string extension = Path.GetExtension(fileName);
+            string tempName = Path.GetFileNameWithoutExtension(fileName);
+            string filePath = Path.Combine(directory, fileName);
+            int n = 1;
+            while (File.Exists(filePath))
+            {
+                fileName = $"{tempName} ({n}){extension}";
+                filePath = Path.Combine(directory, fileName);
+                n++;
+            }
+
+            return filePath;
+        }
+    }
+}
