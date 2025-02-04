0f00543600f1d824e3210cd722e2f0eebf2ff5ec
Anton <anton260903@gmail.com>
2025-02-04 22:20:25 +0200

    Shared project added

---
+++ Shared/Constants/Constants.cs
@@ -0,0 +1,30 @@
+﻿namespace Shared.Constants
+{
+    public static class ServerConstants
+    {
+        public static class FileHub
+        {
+            public const string HubConnected = "HubConnected";
+
+            public const string SendRequest = "SendRequest";
+
+            public const string ReceiveRequest = "ReceiveRequest";
+
+            public const string SendResponse = "SendResponse";
+
+            public const string ReceiveResponse = "ReceiveResponse";
+
+            public const string StartSendingFile = "StartSendingFile";
+
+            public const string StartReceivingFile = "StartReceivingFile";
+
+            public const string SendFile = "SendFile";
+
+            public const string ReceiveFile = "ReceiveFile";
+
+            public const string SenderDisconnected = "SenderDisconnected";
+
+            public const string ReceiverDisconnected = "ReceiverDisconnected";
+        }
+    }
+}
