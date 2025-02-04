a4b9e6198eeebf870c037b5d94b5445262b9e02f
Anton <anton260903@gmail.com>
2025-02-04 22:23:45 +0200

    JS ClipboardHelper class added

---
+++ Client/wwwroot/js/ClipboardHelper.js
@@ -0,0 +1,10 @@
+﻿export class ClipboardHelper {
+
+    static CopyToClipboard(text) {
+        navigator.clipboard.writeText(text);
+    }
+
+    static async ReadFromClipboard() {
+        return await navigator.clipboard.readText();
+    }
+}
