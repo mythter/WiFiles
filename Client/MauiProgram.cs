787f3a402ec923a48029b2d26bc22b9da14c2324
Anton <anton260903@gmail.com>
2025-02-04 22:23:00 +0200

    service configuration moved to MauiProgram.cs

--- Client/MauiProgram.cs
+++ Client/MauiProgram.cs
@@ -1,4 +1,9 @@
-﻿using CommunityToolkit.Maui;
+﻿using System.Reflection;
+using Blazored.LocalStorage;
+using Client.Interfaces;
+using Client.Services;
+using CommunityToolkit.Maui;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Client
@@ -16,16 +21,62 @@ namespace Client
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
+            using var appsettingsStream = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceStream("Client.wwwroot.appsettings.json");
+
+            if (appsettingsStream != null)
+            {
+                var config = new ConfigurationBuilder()
+                    .AddJsonStream(appsettingsStream)
+                    .Build();
+
+                builder.Configuration.AddConfiguration(config);
+            }
+
             builder.Services.AddMauiBlazorWebView();
 
-            builder.Services.AddBusinessLogicLayer();
+            builder.Services.ConfigureServices();
+
+            builder.Services.AddBlazoredLocalStorage();
 
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
+
+            AppDomain.CurrentDomain.FirstChanceException += (s, e) =>
+            {
+                Console.WriteLine("********** UNHANDLED EXCEPTION **********");
+                Console.WriteLine($"Message: {e.Exception.Message}");
+                Console.WriteLine($"Stack trace: {e.Exception.StackTrace}");
+            };
 #endif
 
             return builder.Build();
+        }
+
+        /// <summary>
+        /// Extension method for configuring custom services
+        /// </summary>
+        static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<NavigationService>();
+            services.AddScoped<ILocalNetworkService, LocalNetworkService>();
+
+            services.AddSingleton<LocalTransferService>();
+            services.AddSingleton<GlobalTransferService>();
+
+#if ANDROID
+            services.AddSingleton<INetworkInfoService, AndroidNetworkInfoService>();
+            services.AddSingleton<IDeviceService, AndroidDeviceService>();
+
+            services.AddSingleton<IStorageService, AndroidStorageService>();
+#elif WINDOWS
+            services.AddSingleton<INetworkInfoService, WindowsNetworkInfoService>();
+            services.AddSingleton<IDeviceService, WindowsDeviceService>();
+
+            services.AddSingleton<IStorageService, WindowsStorageService>();
+#endif
         }
     }
 }
