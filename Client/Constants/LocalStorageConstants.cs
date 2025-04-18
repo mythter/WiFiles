ff6bf487e8c40c07eb69237fefd4160babb78af4
Anton <anton260903@gmail.com>
2025-04-18 23:58:32 +0300

    added saving receive files to local storage and also removing received files from history

--- Client/Constants/LocalStorageConstants.cs
+++ Client/Constants/LocalStorageConstants.cs
@@ -3,5 +3,9 @@
 	public static class LocalStorageConstants
 	{
 		public const string ReceiverIdKey = "receiverId";
+
+		public const string SendFilesKey = "sendFiles";
+
+		public const string ReceiveFilesKey = "receiveFiles";
 	}
 }
