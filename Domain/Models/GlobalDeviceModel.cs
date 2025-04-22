08c3ef98e52e6fe074c43a61346ce6fff0de1ccc
Anton <anton260903@gmail.com>
2025-04-22 22:32:52 +0300

    GlobalDeviceModel added

---
+++ Domain/Models/GlobalDeviceModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using Domain.Enums;
+
+namespace Domain.Models
+{
+	public class GlobalDeviceModel : DeviceModel
+	{
+		public GlobalDeviceModel(string name) : base(name)
+		{ }
+
+		public GlobalDeviceModel(DeviceModel deviceModel)
+			: this(
+				deviceModel.Name,
+				deviceModel.Type,
+				deviceModel.Model,
+				deviceModel.Manufacturer)
+		{ }
+
+		[JsonConstructor]
+		public GlobalDeviceModel(
+			string name,
+			DeviceModelType type,
+			string? model,
+			string? manufacturer)
+		: base(name, type, model, manufacturer)
+		{ }
+	}
+}
