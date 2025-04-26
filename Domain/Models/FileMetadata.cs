e608d73c15495b6c06d73571b2952e4aec133913
Anton <anton260903@gmail.com>
2025-04-26 22:54:56 +0300

    FileId added to FileMetadata

--- Domain/Models/FileMetadata.cs
+++ Domain/Models/FileMetadata.cs
@@ -1,13 +1,26 @@
-﻿namespace Domain.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Models
 {
 	public class FileMetadata
 	{
+		public Guid FileId { get; }
+
 		public string Name { get; set; }
 
 		public long Size { get; set; }
 
 		public FileMetadata(string name, long size)
 		{
+			FileId = Guid.NewGuid();
+			Name = name;
+			Size = size;
+		}
+
+		[JsonConstructor]
+		public FileMetadata(Guid fileId, string name, long size)
+		{
+			FileId = fileId;
 			Name = name;
 			Size = size;
 		}
