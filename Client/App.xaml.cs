c0a3c1201f4ff17a6c0daecf515d7e3dd7bf5273
Anton <anton260903@gmail.com>
2025-05-03 00:39:48 +0300

    added startup window sizes on creating

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -8,5 +8,18 @@
 
 			MainPage = new MainPage();
 		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			var window = base.CreateWindow(activationState);
+
+			const int newWidth = 650;
+			const int newHeight = 550;
+
+			window.Width = newWidth;
+			window.Height = newHeight;
+
+			return window;
+		}
 	}
 }
